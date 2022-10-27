a4823b9fec56b0f519ee94b64b43a7f3f6076e08
nicholas <nnnnicholas@users.noreply.github.com>
2022-10-27 19:21:06 -0400

    update

--- src/AddressToENSString.sol
+++ src/AddressToENSString.sol
@@ -5,21 +5,48 @@ pragma solidity ^0.8.0;
 /// @author nnnnicholas
 /// @notice Given an address, return the ENS name associated with it, or fallback to a stringified version of the address if no ENS name is found.
 
+// Reverse Resolution Interfaces
 interface IReverseRegistrar {
     function node(address) external view returns (bytes32);
 }
 
-interface IResolver {
-    function name(bytes32) external view returns (string memory);
+interface IReverseResolver {
+    function name(bytes32) external view returns (string memory); // reverse
+}
+
+interface IForwardResolver {
+    function addr(bytes32 node) external view returns (address); // forward
+}
+
+// Forward Resolution Interfaces
+interface IENS {
+    function resolver(bytes32 node) external view returns (IForwardResolver);
 }
 
 contract AddressToENSString {
     IReverseRegistrar reverseRegistrar =
-        IReverseRegistrar(0x084b1c3C81545d370f3634392De611CaaBFf8148); // mainnet
-    IResolver resolver = IResolver(0xA2C122BE93b0074270ebeE7f6b7292C7deB45047); // mainnet
+        IReverseRegistrar(0x084b1c3C81545d370f3634392De611CaaBFf8148);
+    IReverseResolver reverseResolver =
+        IReverseResolver(0xA2C122BE93b0074270ebeE7f6b7292C7deB45047);
+    IENS ens = IENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
 
     function getName(address _addr) public view returns (string memory) {
-        return resolver.name(reverseRegistrar.node(_addr));
+        // Reverse
+        bytes32 reportedNode = reverseRegistrar.node(_addr);
+        string memory name = reverseResolver.name(reportedNode);
+
+        // Forward
+        IForwardResolver forwardResolver = IForwardResolver(
+            ens.resolver(reportedNode) // TODO BROKEN
+        );
+        address resolvedAddress = forwardResolver.addr(reportedNode);
+
+        // Comparison
+        if (resolvedAddress == _addr) {
+            return name;
+        } else {
+            return "";
+        }
     }
 
     function getNameWithFallback(address _addr)
@@ -27,12 +54,13 @@ contract AddressToENSString {
         view
         returns (string memory)
     {
-        string memory name = resolver.name(reverseRegistrar.node(_addr));
-        if (bytes(name).length > 0) {
-            return name;
-        } else {
-            return string(abi.encodePacked("0x", toAsciiString(_addr)));
-        }
+        // string memory name = resolver.name(reverseRegistrar.node(_addr));
+        // //TODO Forward resolution
+        // if (bytes(name).length > 0) {
+        //     return name;
+        // } else {
+        //     return string(abi.encodePacked("0x", toAsciiString(_addr)));
+        // }
     }
 
     // borrowed from https://ethereum.stackexchange.com/questions/8346/convert-address-to-string
--- test/AddressToENSString.t.sol
+++ test/AddressToENSString.t.sol
@@ -12,6 +12,6 @@ contract AddressToENSStringTest is Test {
 
     function testGetName() public {
         assertEq(c.getName(0xAF28bcB48C40dBC86f52D459A6562F658fc94B1e), "dao.jbx.eth"); // known record
-        assertEq(c.getNameWithFallback(0xA2C122BE93b0074270ebeE7f6b7292C7deB45047), "0xa2c122be93b0074270ebee7f6b7292c7deb45047"); // No ENS name
+        // assertEq(c.getNameWithFallback(0xA2C122BE93b0074270ebeE7f6b7292C7deB45047), "0xa2c122be93b0074270ebee7f6b7292c7deb45047"); // No ENS name
     }
 }
